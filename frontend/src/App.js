54925ee10ec47292f20850d8a73343fac80f099d
OzanCikriklioglu-cloud <ozancikriklioglu@outlook.com>
2025-10-21 22:19:26 +0200

    Initial commit - React frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Code from './code'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Code/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/code.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function code ()
+{
+    return (
+    <h1>Book Review App</h1>
+  );
+}
+
+export default code;
